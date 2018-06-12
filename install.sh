6271ff7cb23d8abeea6ccd77c8b28ba757210e7d
Ozzy <romosborne@gmail.com>
2018-06-12 21:06:37 +0100

    Install powerline fonts

--- install.sh
+++ install.sh
@@ -3,3 +3,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s $DIR/.tmux.conf ~/.tmux.conf
+
+###################
+# Powerline fonts #
+###################
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+
