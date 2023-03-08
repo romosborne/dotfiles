795b9c884978cb5d7a4d51ef47ff73cf382ee055
Ozzy <romosborne@gmail.com>
2023-03-08 22:29:24 +0000

    missing tilde

--- install/rust.sh
+++ install/rust.sh
@@ -2,7 +2,7 @@
 
 if ! command -v cargo > /dev/null; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-  echo '. "$HOME/.cargo/env"' >> .zshrc
+  echo '. "$HOME/.cargo/env"' >> ~.zshrc
 else 
   ok "Already installed"
 fi
