db2e4d61f0ed9c3bf9e5f47ef06cea1a9dc88ca4
Ozzy <romosborne@gmail.com>
2023-03-08 20:39:15 +0000

    Homebrew improvements

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -2,6 +2,8 @@
 
 if ! command -v brew > /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brwe shellenv)"') >> ~/.profile
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 else 
   ok "Already installed"
 fi
